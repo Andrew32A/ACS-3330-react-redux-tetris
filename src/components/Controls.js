feece0486d1cff4a03488d57da3118e98a0ecde6
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-13 14:08:21 -0800

    added connection for controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { moveDown, moveLeft, moveRight, rotate } from "../features/gameSlice";
 
 export default function Controls(props) {
+  const dispatch = useDispatch();
+  const { isRunning } = useSelector((state) => state);
+
   return (
     <div className="controls">
       {/* left */}
       <button
+        disabled={!isRunning}
         className="control-button"
         onClick={(e) => {
-          // ...
+          dispatch(moveLeft());
         }}
       >
         Left
@@ -15,9 +21,10 @@ export default function Controls(props) {
 
       {/* right */}
       <button
+        disabled={!isRunning}
         className="control-button"
         onClick={(e) => {
-          // ...
+          dispatch(moveRight());
         }}
       >
         Right
@@ -25,9 +32,10 @@ export default function Controls(props) {
 
       {/* rotate */}
       <button
+        disabled={!isRunning}
         className="control-button"
         onClick={(e) => {
-          // ...
+          dispatch(rotate());
         }}
       >
         Rotate
@@ -35,9 +43,10 @@ export default function Controls(props) {
 
       {/* down */}
       <button
+        disabled={!isRunning}
         className="control-button"
         onClick={(e) => {
-          // ...
+          dispatch(moveDown());
         }}
       >
         Down
