c7c07c202735dbc807713666e293b6bc482b0f06
Andrew Alsing <andrew32alsing@gmail.com>
2024-02-14 17:14:55 -0800

    finishing touches

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -9,7 +9,7 @@ export default function ScoreBoard() {
   return (
     <div className="score-board">
       <div>Score:{score}</div>
-      <div>Level: 1</div>
+      <div>Level:1</div>
       <button
         className="score-board-button"
         onClick={(e) => {
